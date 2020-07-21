6203176804386a5efee1ffd04003987d05998601
Austin Merrick <austinmerrick98@gmail.com>
2020-07-21 11:11:12 -0700

    Adding backend functionality

--- index.js
+++ index.js
@@ -1,23 +1,177 @@
-function formSubmit() {
+let loadingText = document.getElementById("LoadingText");
+let submitButton = document.getElementById("SubmitButton");
+
+let form = {
+    username: document.getElementById("InputUsername"),
+    firstName: document.getElementById("InputFirstName"),
+    lastName: document.getElementById("InputLastName"),
+    usernameHelp: document.getElementById("UsernameHelp"),
+    firstNameHelp: document.getElementById("FirstNameHelp"),
+    lastNameHelp: document.getElementById("LastNameHelp"),
+    submitSuccessText: document.getElementById("SubmitSuccess"),
+    submitFailText: document.getElementById("SubmitFail"),
+}
+
+let loadingTexts = ["Loading","Loading.","Loading.."];
+let curLoad = 0
+
+const apiUrl = "http://localhost:3000";
+
+function getEntries() {
     const options = {
-        method: 'POST',
+        method: 'GET',
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({
-            name: "testing"
-        }),
-    }
+    };
+
+    document.getElementById("EntryHolder").innerHTML = "";
 
 
-    fetch('http://localhost:3000/submit', options)
+    fetch( apiUrl+"/entries", options )
         .then(response => response.json())
         .then(json => {
-            //done loading
-            console.log(json);
+            //loading entries!
+
+            const message = JSON.parse(json)
+
+            if (message.status == "success")
+            {
+                for (let row of message.rows)
+                {
+                    /*
+                    <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">jang808</h5>
+                    <p class="card-text">First Name: Harry</p>
+                    <p class="card-text">Last Name: Harry</p>
+                </div>
+            </div>
+                    */
+
+                    let newHTML = "<div class='card'><div class='card-body'>";
+                    newHTML += "<h5 class='card-title'>" + row.username + "</h5>";
+                    newHTML += "<hr/>";
+                    newHTML += "<p class='card-text'> First Name: " + row.firstname + "</p>";
+                    newHTML += "<p class='card-text'> Last Name: " + row.lastname + "</p>";
+                    newHTML += "</div></div>"
+
+                    document.getElementById("EntryHolder").innerHTML += newHTML;
+                }
+            }
+            else
+            {
+                console.log(message);
+                //failed
+            }
+
         })
         .catch((error) => {
+            //failed
             console.error("Error:", error);
             //there was a problem!
         });
+}
+
+getEntries();
+
+function changeLoadingText() {
+    curLoad += 1;
+    loadingText.textContent = loadingTexts[curLoad%loadingTexts.length];
+    setTimeout(changeLoadingText, 500);
+}
+
+//this cycles the Loading text so user knows page has not froze
+changeLoadingText();
+
+function startLoading() {
+    submitButton.style.display = "none";
+    loadingText.style.display = "block";
+    form.submitFailText.style.display = "none";
+    form.submitSuccessText.style.display = "none";
+}
+
+function doneLoading() {
+    submitButton.style.display = "block";
+    loadingText.style.display = "none";
+}
+
+function formValidate() {
+
+    form.firstNameHelp.style.display = "none";
+    form.lastNameHelp.style.display = "none";
+    form.usernameHelp.style.display = "none";
+
+    if (form.firstName.value && form.lastName.value && form.username.value)
+    {
+        return true;
+    }
+    else 
+    {
+        if (!form.firstName.value)
+        {
+            form.firstNameHelp.style.display = "block";
+        }
+        if (!form.lastName.value)
+        {
+            form.lastNameHelp.style.display = "block";
+        }
+        if (!form.username.value)
+        {
+            form.usernameHelp.style.display = "block";
+        }
+        return false;
+    }
+}
+
+function formSubmit() {
+
+    startLoading();
+    
+    if ( formValidate() )
+    {
+
+        const options = {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                firstname: form.firstName.value,
+                lastname: form.lastName.value,
+                username: form.username.value
+            }),
+        }
+    
+    
+        fetch( apiUrl+"/submit", options )
+            .then(response => response.json())
+            .then(json => {
+                //done loading
+                doneLoading();
+    
+                const message = JSON.parse(json)
+    
+                if (message.status == "success")
+                {
+                    form.submitSuccessText.style.display = "block";
+                    getEntries();
+                }
+                else
+                {
+                    form.submitFailText.style.display = "block";
+                }
+    
+            })
+            .catch((error) => {
+                doneLoading();
+                console.error("Error:", error);
+                //there was a problem!
+            });
+    }
+    else
+    {
+        doneLoading();
+    }
+
 }
--- server.js
+++ server.js
@@ -1,32 +1,73 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+require('dotenv').config();
 
+const connectionString = process.env.DB_STRING;
 
-//commented out code will be brought in for database later
-
-// const Pool = require('pg').Pool;
-// require('dotenv').config();
-
-// const pool = new Pool({
-//     connectionString: process.env.DATABASE_URL
-// })
+const { Pool, Client } = require('pg')
+const pool = new Pool({
+    connectionString: connectionString,
+})
 
 const port = process.env.PORT || 3000;
 
-app.use(express.json({limit:'1mb'}));
+app.use(express.json({ limit: '1mb' }));
 
 //CORS so that client be hosted seperately from the server
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
 app.post('/submit', (request, response) => {
-    console.log(request.body);
-    console.log("submit post triggered");
-    //request.body has the form info
+
+    const userInfo = request.body;
+
+    let queryString = "INSERT INTO Entries VALUES (";
+    queryString += "'" + userInfo.lastname + "',";
+    queryString += "'" + userInfo.firstname + "',";
+    queryString += "'" + userInfo.username + "');";
+
+    pool.query(queryString, (err, res) => {
+        if (err) {
+            console.log(err.stack);
+        } else {
+            console.log("added.");
+
+            const validationMessage = {
+                status: "success"
+            };
+
+            response.json(JSON.stringify(validationMessage));
+        }
+    });
+
 });
 
-app.listen(port, () => console.log(`Example app listening on port localhost:${port}`));
+app.get('/entries', (request, response) => {
+
+    pool.query('SELECT LastName, FirstName, Username FROM Entries;', (err, res) => {
+        if (err) {
+            console.log(err.stack);
+
+            const validationMessage = {
+                status: "failure"
+            };
+
+            response.json(JSON.stringify(validationMessage));
+
+        } else {
+            const validationMessage = {
+                status: "success",
+                rows: res.rows
+            };
+
+            response.json(JSON.stringify(validationMessage));
+        }
+    });
+
+});
+
+app.listen(port, () => console.log(`App listening on port localhost:${port}`));
